3a16ae52d4334e81138f74470633e002b09703b5
Tobias Oetiker <tobi@oetiker.ch>
2011-06-03 21:29:50 +0200

    *** INCOMPATIBLE CHANGES *** The QooxdooJsonrpc plugin does not install a /source path automatically anymore ... there were too many problem with other parts of the application which would need special attention to work with relative requests coming from a different 'path' ... starting with this version, there are now two environment variables that govern source mode:
    QX_SRC_MODE=1 will enable serving your application from
    ../frontend/source ...
    
    QX_SRC_PATH=/where/ever/source allows to specify some other
    directory to serve your application from.

--- lib/Mojolicious/Plugin/QooxdooJsonrpc.pm
+++ lib/Mojolicious/Plugin/QooxdooJsonrpc.pm
@@ -14,36 +14,41 @@ sub register {
 
     # Config
     $conf ||= {};
+    my $root = ($conf->{prefix} || '') . '/';
     my $services = $conf->{services};
-    my $path = $conf->{path} || '/jsonrpc';
-    my $qx_app_src = $conf->{qx_app_src} || $app->home->rel_dir('../frontend');
+    my $path = $conf->{path} || 'jsonrpc';
     my $r = $app->routes;
 
-    if ($app->mode eq 'development'){
-        $r->route('/source'.$path)->to(
-            class       => 'Jsonrpc',
-            method      => 'dispatch',
-            namespace   => 'MojoX::Dispatcher::Qooxdoo',        
-            # our own properties
-            services    => $services,        
-            debug       => 1,        
-        );
+    $r->route($root.$path)->to(
+        class       => 'Jsonrpc',
+        method      => 'dispatch',
+        namespace   => 'MojoX::Dispatcher::Qooxdoo',        
+        # our own properties
+        services    => $services
+    );
 
+    if ($ENV{QX_SRC_MODE}){
+        $app->log->info("Enabeling source mode");
+        my $qx_app_src = $app->home->rel_dir($ENV{QX_SRC_PATH} || catdir(updir,'frontend','source'));
+        $app->log->info("Serving source files from $qx_app_src");
         # relative path
         my $rel_static = Mojolicious::Static->new();
         $rel_static->root($qx_app_src);
         my $rel_static_cb = sub {
              my $self = shift;
-             if (not $self->stash->{file}){
-                 $self->req->url->path($self->req->url->path.'index.html');
-             }
+             my $file = $self->stash->{file} || 'index.html';
+             $self->req->url->path('/'.$file); # relative paths get appended ... 
              return $rel_static->dispatch($self);
         };
-        $r->get('/source/(*file)' => $rel_static_cb );
-        $r->get('/source/' => $rel_static_cb );
+        
+        my @root = split /\//, $root;
+        pop @root;
+        $r->get(join('/',@root).'/source/(*file)' => $rel_static_cb );
+        $r->get($root.'(*file)' => $rel_static_cb );
+        $r->get($root => $rel_static_cb );
 
-        my $abs_static = Mojolicious::Static->new();
         my %prefixCache;
+        my $abs_static = Mojolicious::Static->new();
         my $abs_static_cb = sub {
             my $self = shift;
             my $prefix = $self->stash->{prefix};
@@ -55,7 +60,7 @@ sub register {
                     $last_path = $path;
                     $path = abs_path(catdir($last_path,updir));
                 }
-                $self->app->log->info("Auto register static path mapping from '$prefix' to '".catdir($path,$prefix_local)."'");
+                $app->log->info("Auto register static path mapping from '$prefix' to '".catdir($path,$prefix_local)."'");
                 $prefixCache{$prefix} = $path;
             } 
             $abs_static->root($prefixCache{$prefix});
@@ -64,16 +69,16 @@ sub register {
 
         $r->get('/(*prefix)/framework/source/(*b)' => $abs_static_cb );
         $r->get('/(*prefix)/downloads/(*b)/source/(*c)' => $abs_static_cb );
-
-    };
-    $r->route($path)->to(
-        class       => 'Jsonrpc',
-        method      => 'dispatch',
-        namespace   => 'MojoX::Dispatcher::Qooxdoo',        
-        # our own properties
-        services    => $services
-    );
-    $r->get( '/' => sub { shift->render_static('index.html') });
+    }
+    else {
+        $r->get($root.'(*file)' => sub {
+             my $self = shift;
+             my $file = $self->stash->{file};        
+             $self->req->url->path('/'.$file);
+             return $app->static->dispatch($self);
+        });
+        $r->get($root => sub { shift->render_static('index.html') });
+     }
 }
 
 1;
@@ -95,6 +100,8 @@ Mojolicious::Plugin::QooxdooJsonrpc - handle qooxdoo Jsonrpc requests
     my $self = shift;
        
     $self->plugin('qooxdoo_json_rpc',{
+        prefix => '/',
+        path => 'jsonrpc',
         services => {
             Test => RpcService->new(),
         },
@@ -105,9 +112,8 @@ Mojolicious::Plugin::QooxdooJsonrpc - handle qooxdoo Jsonrpc requests
 =head1 DESCRIPTION
 
 This plugin installs the L<MojoX::Dispatcher::Qooxdoo::Jsonrpc> dispatcher
-into your application. If the application is running in development mode the
-plugin will serve both a compiled as well the source version of the
-application. Access the source version via the F</source> url.
+into your application. The result of your build run is expected in the
+F<public/> directory of your mojo app.
 
 See the documentation on L<MojoX::Dispatcher::Qooxdoo::Jsonrpc>
 for details on how to write your service. 
@@ -116,22 +122,41 @@ The plugin understands the following parameters.
 
 =over
 
+=item B<prefix> 
+
+By default the plugin will add its routes starting at the root F</> by setting a prefix
+you can move them down the tree.
+
 =item B<services> (mandatory)
 
 A pointer to a hash of service instances. See L<MojoX::Dispatcher::Qooxdoo::Jsonrpc> for details on how
 to write a service.
 
-=item B<path> (default: /jsonrpc)
+=item B<path> (default: jsonrpc)
 
 If your application expects the JsonRPC service to appear under a different url.
 
-=item B<qx_app_root> (default: the F<../frontend>)
-
-When the mojo application is running in development mode, provide access to the source version
-of the qooxdoo application under the F</source> url. By default it is expected to reside in
-a directory called F<frontend> sitting next to the mojo application directory.
-
 =back
+
+=head2 Source Mode
+
+While developing your qooxdoo application it is handy to run its souce
+version. Normally this is done directly from the file system, but in that
+mode your application will not be able to call back to the server with POST requests.
+
+This module provides a qooxdoo B<source> mode where it will serve the source
+version of your application. Set the C<QX_SRC_MODE> environment variable to
+"1" to activate the source mode. By default, the module expects to find the
+source of your application in F<MOJO_HOME/../frontend/source> if you keep
+the source somewhere else you can set the alternate location via
+C<QX_SRC_PATH> either absolute or relative to the MOJO_HOME directory.
+
+
+
+In production mode, the plugin expects to find the result of your
+C<generate.py build> run in mojos F<public> directory. 
+
+
 
 =head1 AUTHOR
 
